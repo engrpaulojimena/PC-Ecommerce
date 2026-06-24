5c10893a242b3b8d8febb3dba5fca0a4115d1e98
pjimena <pjimena@M0016>
2026-06-24 11:51:55 +0800

    Update project

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { sql } from "@/lib/db";
 import { notFound } from "next/navigation";
 import { Category, Product } from "@/lib/types";
--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { sql } from "@/lib/db";
 import { Category } from "@/lib/types";
 import AdminProductForm from "@/components/AdminProductForm";
